30ef6c70049a29e8a2155300651ca95279df9c36
SAIKAT12000448 <73012048+SAIKAT12000448@users.noreply.github.com>
2024-04-24 22:29:40 +0600

    sss

--- src/Chatbot/Chat.js
+++ src/Chatbot/Chat.js
@@ -1,43 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import io from 'socket.io-client'
 
 // let socket;
 
 const Chat = () => {
     // const [name, setName] = useState('');
+    const [messages, setMessages] = useState([]);
     const [message, setMessage] = useState("");
+    const [room, setRoom] = useState("");
+    const [ socketId, setSocketId] = useState("");
+    const socket = useMemo(()=>io('localhost:4000'),[]);
   
-    const socket = io('localhost:5000');
 
     const handleSubmit=(e)=>{
         e.preventDefault();
-        socket.emit('message',message);
-        setMessage(" ");
+        socket.emit('message',{messages,room});
+        socket.emit('messages',message);
+        message("");
     }   
 
 
 
 
 
-    function connectSocket(){
-        socket.on("connection",(socket)=>{
-            console.log(socket);
-        })
-
-        // return ()=>{
-        //     socket.disconnect();
-        // }
-        
-    }
     useEffect(() => {
-        connectSocket();
+        socket.on("connect", () => {
+          setSocketId(socket.id);
+          console.log("connected", socket.id);
+        });
+    
+        socket.on("personal-message", (data) => {
+          console.log(data);
+          setMessages((messages) => [...messages, data]);
+        });
+    
+        socket.on("message", (s) => {
+          console.log(s);
+        //   setMessage(s);
+        });
+    
+        return () => {
+          socket.disconnect();
+        };
+      }, []);
 
-       
-    });
-
-    socket.on('receive-message',message=>{
-        console.log(message);
-    })
+    
 
     // const handleSend=()=>{
     //     var msg = document.getElementById("msg").value;
@@ -55,16 +62,37 @@ const Chat = () => {
     
 
     return (
-        <div>
+        <div style={{width:'50%', textAlign:'center',marginLeft:'0',marginRight:'0'}}>
             <h1>Chat</h1>
+
+
+            <h5>
+                {
+                    // socketId
+                    
+                }
+            </h5>
             <form onSubmit={handleSubmit} action="">
             <input
+            value={message}
             onChange={(e)=>setMessage(e.target.value)}
-            id='msg' type="text" placeholder='Enter Message' />            
+            id='msg' type="text" placeholder='Enter Message' />    
+
+            <input
+            onChange={(e)=>setRoom(e.target.value)}
+            value={room}
+            id='msg' type="text" placeholder='Enter Room' />            
             <button type='submit'>Send</button> 
             </form>
-            {/* <input id='msg' type="text" placeholder='Enter Message' />            
-            <button onClick={handleSend}>Send</button> */}
+           <p>
+            {
+                 messages.map((m, i) => (
+                    <p key={i} variant="h6" component="div" gutterBottom>
+                      {m}
+                    </p>
+                  ))
+            }
+           </p>
         </div>
     );
 };
